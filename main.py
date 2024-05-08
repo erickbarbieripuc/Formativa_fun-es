8977ae04e69a02afc6c84ef41fb9d0e431768929
erickbarbieripuc <167023009+erickbarbieripuc@users.noreply.github.com>
2024-05-07 22:00:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+# # Atividade 1
+# def dobro(numero):
+#     return numero * 2
+#
+# print(dobro(5))
+
+# # Atividade 2
+# import math
+# def raiz_quadrada(numero):
+#     return math.sqrt(numero)
+#
+# print(raiz_quadrada(25))
+
+# # Atividade 3
+# def maximo(a, b):
+#     if a > b:
+#         print(f"{a} é maior que {b}")
+#     elif b > a:
+#         print(f"{b} é maior que {a}")
+#     else:
+#         print("Os números são iguais")
+#
+# numeros = maximo(3,5)
+# numeros = maximo(6,3)
+# numeros = maximo(3,3)
+
+# # Atividade 4
+# def soma_lista(numeros):
+#     return sum(numeros)
+#
+# lista = [1,2,3,4,5,]
+# soma_numeros = soma_lista(lista)
+# print(soma_numeros)
+
+# # Atividade 5
+# def media(numeros):
+#     return sum(numeros) / len(numeros) if len(numeros) > 0 else 0
+#
+# numeros = [2,4,6]
+# media_numeros = media(numeros)
+# print(media_numeros)
+
+# # Atividade 6
+# def sinal(numero):
+#     if numero > 0:
+#         print("P")
+#     else:
+#         print("N")
+#
+# sinal(1)
+# sinal(-1)
+# sinal(0)
+
+# #Atividade 7
+# def somaimposto(taxaimposto,custo):
+#     #Calcular taxa de imposto
+#     imposto = custo * (taxaimposto / 100)
+#     #Valor da venda com imposto
+#     valorTotal = custo + imposto
+#     print(f"O valor do produto com imposto é {valorTotal}")
+#
+# somaimposto(5,100)
+
+
+
+
+
+
+
+
+
